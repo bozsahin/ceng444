21a7cec2c3b015db701a1072f65a2cbda4c6aab5
bozsahin <cem.bozsahin@gmail.com>
2018-10-18 11:49:54 +0300

    eliminating greyed out module

--- lab-material/antLR-material/balanced-parentheses/EvalVisitor.java
+++
@@ -1,7 +0,0 @@
-public static class EvalVisitor extends LExprBaseVisitor<Integer> { public Integer visitMult(LExprParser.MultContext ctx) {
-return visit(ctx.e(0)) * visit(ctx.e(1)); }
-public Integer visitAdd(LExprParser.AddContext ctx) { return visit(ctx.e(0)) + visit(ctx.e(1));
-}
-public Integer visitInt(LExprParser.IntContext ctx) { return Integer.valueOf(ctx.INT().getText());
-} }
-
--- lab-material/antLR-material/balanced-parentheses/bp.workflow
+++
@@ -1,6 +0,0 @@
-// this is what i did to get it up and running -cemB
-antlr4 bp.g4
-javac bp*.java
-grun bp prog -gui data2
-grun bp prog -tokens data2   // shows the tokens. nice for debugging (if token is not what you think, look at internal match)
-grun bp prog -tree data2     // shows the parse tree, and in Lisp format! how convenient
