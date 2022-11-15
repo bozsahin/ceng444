f30e865f7ce90e9b95ac5682479a5155c6a48531
bozsahin <cem.bozsahin@gmail.com>
2022-11-15 13:58:56 +0300

    simple description of decorators

---
+++ lab-material/sly-material/cb-sly-example/deco.py
@@ -0,0 +1,22 @@
+# decorator test-- general case, any number of args -cb
+
+def d(fun):
+    def inn(*args, **kwargs):
+        print("here is the input")
+        fun(*args, **kwargs)
+        print("done")
+    return inn             # must always return an abstracted function
+
+@d
+def f():
+    print('f does this')
+
+# this is equilavent to f=d(f)
+
+@d
+def h(x,y):
+    return x+y
+
+@d
+def g(x,y,z):
+    print(x+y-z)
